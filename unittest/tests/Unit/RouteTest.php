982f4809ef143242e165a5dd9f71765c395a08bc
RUSHA <rushana.ahmed@g.bracu.ac.bd>
2020-09-29 05:26:41 +0600

    second commit

---
+++ unittest/tests/Unit/RouteTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RouteTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response =$this->get("/");
+        $response->assertStatus(200);
+    }
+}
